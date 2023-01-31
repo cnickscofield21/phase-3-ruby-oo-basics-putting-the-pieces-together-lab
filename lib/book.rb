02c5a9afe89e4dd026e44d4655d85a86caeccf91
Nick Scofield <c.nick.scofield@gmail.com>
2023-01-31 11:33:01 -0700

    commit tree 8ea48fb32ca7d9eb3697dabf8607796d1c0d981d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+require "pry"
 
+class Book
+
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title, author = "Anonymous", page_count = 0, genre = "None")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+require "pry"
+
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand, color = "", size = 1, material = "", condition = "")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        self.condition= "new"
+        puts "Your shoe is as good as new!"
+    end
+
+end
+
+# nike = Shoe.new("Nike", "red")
+# binding.pry
